73b46b2529a64ea807e1443566e358798da128aa
Sivaram Yadav <nsr.learnings@gmail.com>
2023-07-22 19:18:18 +0530

    Added changes

--- LeetCode-by-NSR.playground/Pages/485_MaxConsecutiveOnes.xcplaygroundpage/Contents.swift
+++ LeetCode-by-NSR.playground/Pages/485_MaxConsecutiveOnes.xcplaygroundpage/Contents.swift
@@ -3,15 +3,16 @@ import Foundation
 func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
     var globalCount = 0
     var localCount = 0
+    
     for num in nums {
         if num == 1 {
             localCount += 1
-        } else {
             globalCount = max(globalCount, localCount)
+        } else {
             localCount = 0
         }
     }
-    globalCount = max(globalCount, localCount)
+    
     return globalCount
 }
 
