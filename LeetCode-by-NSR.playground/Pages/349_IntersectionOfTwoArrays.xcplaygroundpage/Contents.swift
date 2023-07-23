09634f1fdbed17b2df67cdff28eb634446299137
Sivaram Yadav <nsr.learnings@gmail.com>
2023-07-23 20:16:09 +0530

    Added changes

--- LeetCode-by-NSR.playground/Pages/349_IntersectionOfTwoArrays.xcplaygroundpage/Contents.swift
+++ LeetCode-by-NSR.playground/Pages/349_IntersectionOfTwoArrays.xcplaygroundpage/Contents.swift
@@ -14,3 +14,25 @@ func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
 
 intersection([1,2,2,1], [2,2]) // [2]
 intersection([4,9,5], [9,4,9,8,4]) // [9,4]
+
+
+
+// Approach 2:
+func intersection2(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+    var set1: Set<Int> = []
+    var set2: Set<Int> = []
+
+    for num in nums1 {
+        set1.insert(num)
+    }
+    
+    for num in nums2 {
+        set2.insert(num)
+    }
+
+    return Array(set1.intersection(set2))
+}
+
+intersection2([1,2,2,1], [2,2]) // [2]
+intersection2([4,9,5], [9,4,9,8,4]) // [9,4]
+
