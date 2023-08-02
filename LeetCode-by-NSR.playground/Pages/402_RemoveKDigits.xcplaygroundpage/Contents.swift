158929d53b379550e93bd9a41982016e7da3d709
Sivaram Yadav <nsr.learnings@gmail.com>
2023-08-02 11:37:17 +0530

    Added 2nd Approach for remove k digits so that get min number

--- LeetCode-by-NSR.playground/Pages/402_RemoveKDigits.xcplaygroundpage/Contents.swift
+++ LeetCode-by-NSR.playground/Pages/402_RemoveKDigits.xcplaygroundpage/Contents.swift
@@ -33,3 +33,37 @@ func removeKdigits(_ num: String, _ k: Int) -> String {
 removeKdigits("1432219", 3) // "1219"
 removeKdigits("10200", 1) // "200"
 removeKdigits("10", 2) // "0"
+
+
+// Approach 2:
+func removeKdigits_v2(_ num: String, _ k: Int) -> String {
+    if num.count == k { return "0" }
+    
+    var numArr: [Character] = Array(num)
+    
+    for _ in 0..<k {
+        var i = 0
+        while i < numArr.count-1 && numArr[i] <= numArr[i+1] {
+            i += 1
+        }
+        numArr.remove(at: i)
+    }
+    
+    if numArr.isEmpty { return "0" }
+    
+
+    if let val = Int(String(numArr)), val == 0 {
+        return "0"
+    }
+
+    var j = 0
+    while numArr[j] == "0" {
+        j += 1
+    }
+
+    return String(numArr[j..<numArr.count])
+}
+
+removeKdigits_v2("1432219", 3) // "1219"
+removeKdigits_v2("10200", 1) // "200"
+removeKdigits_v2("10", 2) // "0"
