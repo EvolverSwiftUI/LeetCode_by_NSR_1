becccbf6d9b39cd9f16bfaada7d8106474304b06
Sivaram Yadav <nsr.learnings@gmail.com>
2023-08-03 11:10:58 +0530

    Added test cases

--- LeetCode-by-NSR.playground/Pages/121_BestTimeToBuyAndSellStock.xcplaygroundpage/Contents.swift
+++ LeetCode-by-NSR.playground/Pages/121_BestTimeToBuyAndSellStock.xcplaygroundpage/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+import XCTest
 
 func maxProfit(_ prices: [Int]) -> Int {
     if prices.isEmpty { return 0 }
@@ -19,3 +20,32 @@ func maxProfit(_ prices: [Int]) -> Int {
 
 maxProfit([7,1,5,3,6,4]) // 5
 maxProfit([7,6,4,3,1]) // 0
+
+class Test: XCTestCase {
+    func testCase1() {
+        let actual = maxProfit([7,1,5,3,6,4])
+        let expected = 5
+        XCTAssertEqual(expected, actual)
+    }
+    
+    func testCase2() {
+        let actual = maxProfit(([7,6,4,3,1]))
+        let expected = 0
+        XCTAssertEqual(expected, actual)
+    }
+}
+
+Test.defaultTestSuite.run()
+
+/*
+ Test Results - as PASSED All cases:
+ 
+ Test Suite 'Test' started at 2023-08-03 11:03:52.583
+ Test Case '-[__lldb_expr_3.Test testCase1]' started.
+ Test Case '-[__lldb_expr_3.Test testCase1]' passed (0.004 seconds).
+ Test Case '-[__lldb_expr_3.Test testCase2]' started.
+ Test Case '-[__lldb_expr_3.Test testCase2]' passed (0.000 seconds).
+ Test Suite 'Test' passed at 2023-08-03 11:03:52.591.
+      Executed 2 tests, with 0 failures (0 unexpected) in 0.004 (0.007) seconds
+
+ */
