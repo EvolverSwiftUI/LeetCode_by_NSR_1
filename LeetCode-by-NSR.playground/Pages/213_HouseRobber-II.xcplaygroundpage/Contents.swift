85402f7766c628f7e0dbfc2de76ef628576f355e
Sivaram Yadav <nsr.learnings@gmail.com>
2023-08-19 21:20:00 +0530

    213_HouseRobber-II

---
+++ LeetCode-by-NSR.playground/Pages/213_HouseRobber-II.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func rob(_ nums: [Int]) -> Int {
+    
+    if nums.count < 3 { return nums.max()! }
+    
+    let housesA = Array(nums[0...nums.count-2])
+    let housesB = Array(nums[1...nums.count-1])
+    
+    func maxMoney(_ houses: [Int]) -> Int {
+        var maxMoney = Array(repeating: 0, count: houses.count)
+        maxMoney[0] = houses[0]
+        maxMoney[1] = max(houses[0], houses[1])
+        
+        for i in 2..<houses.count {
+            maxMoney[i] = max(maxMoney[i-2] + houses[i], maxMoney[i-1])
+        }
+        
+        return maxMoney.last!
+    }
+    
+    let maxA = maxMoney(housesA)
+    let maxB = maxMoney(housesB)
+    
+    return max(maxA, maxB)
+}
+
+
+rob([2,3,2]) // 3
+rob([1,2,3,1]) // 4
+rob([1,2,3]) // 3
+
