e9d85af5ceeee248d0dd765777841ce3ac00506f
Sivaram Yadav <nsr.learnings@gmail.com>
2023-09-05 13:13:30 +0530

    Added changes using proper binary search

--- LeetCode-by-NSR.playground/Pages/35_SearchInsertPosition.xcplaygroundpage/Contents.swift
+++ LeetCode-by-NSR.playground/Pages/35_SearchInsertPosition.xcplaygroundpage/Contents.swift
@@ -2,29 +2,26 @@ import Foundation
 
 func searchInsert(_ nums: [Int], _ target: Int) -> Int {
     
-    var index = 0
+    var index = nums.count // if target is beyond upper bound
     var first = 0
     var last = nums.count - 1
     
     while first <= last {
-        var mid = (last-first)/2 + first
+        var mid = (last+first)/2
         let num = nums[mid]
-        if target < num {
-            last = mid - 1
-        } else if target > num {
-            first = mid + 1
-        } else {
+        // may be an answer
+        if num >= target {
             index = mid
-            break
+            last = mid - 1 // look for more small index on left
+        } else  {
+            first = mid + 1 // look for right
         }
-   }
-    
-    index = first
-    
+    }
+     
     return index
 }
 
 
-//searchInsert([1,3,4,5], 5) // 2
+searchInsert([1,3,4,5], 5) // 3
 searchInsert([1,3,5,6], 2) // 1
-//searchInsert([1,3,5,6], 7) // 4
+searchInsert([1,3,5,6], 7) // 4
